6fb7018a64032a05bb194030a1131d1443779eb2
allowwidth <k30114@gmail.com>
2022-05-12 05:26:32 +0100

    remove rescle

--- main.py
+++ main.py
@@ -1,66 +1,56 @@
-# %%
+
 from venv import create
 import cv2
 from PIL import Image
 import pytesseract
 import numpy as np
-# %%
+import time
+from onnxruntime_infer.rapid_ocr_api import TextSystem
+
 def show_img(name, img):
     cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def sharpen(img, sigma=100):    
-    # sigma = 5、15、25
-    blur_img = cv2.GaussianBlur(img, (0, 0), sigma)
-    usm = cv2.addWeighted(img, 1.5, blur_img, -0.5, 0)
-    return usm
-
 def cropImg(img, boxes, size):
-    (H, W) = size
-    h_s = H/416
-    w_s = W/416
+    #(H, W) = size
+    #h_s = H/416
+    #w_s = W/416
     x,y,w,h = boxes
     crop_img = img[y:y+h, x:x+w]
-    mx = max(crop_img.shape[:2])
-    scale = 300/mx
-    crop_img = cv2.resize(crop_img, None, fx=scale*w_s, fy=scale*h_s)
+    #mx = max(crop_img.shape[:2])
+    #scale = 300/mx
+    #crop_img = cv2.resize(crop_img, None, fx=scale*w_s, fy=scale*h_s)
     return crop_img
 
 
 
-# %%
-
-weight_path = r"./yolov4/yolov4-tiny-obj_best.weights"
-cfg_path = r"./yolov4/yolov4-tiny-obj.cfg"
-
-# %%
-net = cv2.dnn.readNet(weight_path, cfg_path)
-model = cv2.dnn_DetectionModel(net)
-model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
-
-# %%
-def rapidOCR(name, box):
-    from onnxruntime_infer.rapid_ocr_api import TextSystem, visualize
+def setup():
+    weight_path = r"./yolov4/yolov4-tiny-obj_best.weights"
+    cfg_path = r"./yolov4/yolov4-tiny-obj.cfg"
 
     det_model_path = './onnxruntime_infer/models/ch_ppocr_mobile_v2.0_det_infer.onnx'
     cls_model_path = './onnxruntime_infer/models/ch_ppocr_mobile_v2.0_cls_infer.onnx'
 
-    # 中英文识别
     rec_model_path = './onnxruntime_infer/models/en_number_mobile_v2.0_rec_infer.onnx'
     keys_path = './onnxruntime_infer/rec_dict/en_dict.txt'
+
+    net = cv2.dnn.readNet(weight_path, cfg_path)
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
     text_sys = TextSystem(det_model_path,
                         rec_model_path,
                         use_angle_cls=True,
                         cls_model_path=cls_model_path,
                         keys_path=keys_path)
+    return model, text_sys
 
 
+def rapidOCR(name, box, text_sys):
     dt_boxes, rec_res = text_sys(name, box)
     return rec_res
 
-# %%
 
 def create_boxes(bshape):
     x, y = bshape[1] ,bshape[0]
@@ -71,11 +61,8 @@ def create_boxes(bshape):
     box[0][3] = np.array([0, y])
     return box
 
-img_foloer = './images/'
-img_list = ['car1.jpg', 'car1.jpg', 'test.jpg']
-for i in range(1):
-    img = cv2.imread(img_foloer+img_list[i])
-    # print(img.shape)
+def ALPR(model, text_sys, image_path):
+    img = cv2.imread(image_path)
     size = img.shape[:2]
     img = cv2.resize(img, (416, 416), interpolation=cv2.INTER_AREA)
     res = model.detect(img, 0.3, 0.2)
@@ -83,15 +70,21 @@ for i in range(1):
         classes, confidence, boxes = res
         classes, confidence, boxes = classes[0], confidence[0], boxes[0]
         crop_img = cropImg(img, boxes, size)
-        # box = create_boxes(crop_img.shape)
         box = []
     else:
         print("could not detect number plate")
-        continue
+        return
+        
     name = 'croped.jpg'
     #show_img('croped', crop_img)
     cv2.imwrite(name, crop_img)
-    print(rapidOCR(name, box))
+    print(rapidOCR(name, box, text_sys))
 
+if __name__ == '__main__':
+    model, text_sys = setup()
+    img_foloer = './images/'
+    img_list = ['car.jpg', 'car4.jpg', 'test.jpg']
+    for i in range(3):
+        ALPR(model, text_sys, (img_foloer+img_list[i]))
 
 
